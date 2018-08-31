c6045d4c1079e93f99d56839f70977b905c67963
Jure Sotosek <jure.sotosek@gmail.com>
2018-08-31 18:48:03 +0200

    Removed saved

--- src/pages/saved.js
+++ src/pages/saved.js
@@ -1,24 +1,5 @@
 import React from 'react';
 
-import GithubPopup from '../lib/auth/githubPopup';
-
-const Saved = () => {
-  const handleLogin = () => {
-    const clientId = 'ca97343db8b729b071dd';
-    const scope = 'read:user';
-    const popup = GithubPopup.open({
-      client_id: clientId,
-      scope
-    });
-
-    popup.then(console.log, console.log);
-  };
-
-  return (
-    <div>
-      <button onClick={handleLogin}>hej</button>
-    </div>
-  );
-};
+const Saved = () => <div />;
 
 export default Saved;
