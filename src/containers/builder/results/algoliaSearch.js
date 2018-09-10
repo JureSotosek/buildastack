67094253798d178c85a15af401350d8f9d92c94c
Jure Sotosek <jure.sotosek@gmail.com>
2018-09-10 20:40:14 +0200

    Added stalledSearch

--- src/containers/builder/results/algoliaSearch.js
+++ src/containers/builder/results/algoliaSearch.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {
   InstantSearch,
   Configure,
-  connectHits,
   connectSearchBox,
   connectStateResults
 } from 'react-instantsearch-dom';
@@ -13,43 +12,45 @@ import ResultPlaceholder from '../../../components/ResultPlaceholder';
 const VirtualSearchBox = connectSearchBox(() => null);
 
 const ContentWithSelect = onSelect =>
-  connectStateResults(({ searching, error, searchState, searchResults }) => {
-    if (searchState.query.length === 0) {
-      return <ResultPlaceholder msg={'Start typing to get results 📝'} />;
-    }
+  connectStateResults(
+    ({ searching, isSearchStalled, error, searchState, searchResults }) => {
+      if (searchState.query.length === 0) {
+        return <ResultPlaceholder msg={'Start typing to get results 📝'} />;
+      }
 
-    if (searching) {
-      return <ResultPlaceholder loading />;
-    }
+      if (searching && isSearchStalled) {
+        return <ResultPlaceholder loading />;
+      }
 
-    if (error) {
-      return <ResultPlaceholder error />;
-    }
+      if (error) {
+        return <ResultPlaceholder error />;
+      }
 
-    if (searchResults.hits.length === 0) {
-      return <ResultPlaceholder msg={'Sorry, search yielded nothing 😕'} />;
-    }
+      if (searchResults.hits.length === 0) {
+        return <ResultPlaceholder msg={'Sorry, search yielded nothing 😕'} />;
+      }
 
-    const hits = searchResults.hits;
-    return hits.map(hit => (
-      <ResultCard
-        key={hit.name + hit.version}
-        hit={hit}
-        onSelect={() => onSelect(hit, false)}
-        onSelectDev={() => onSelect(hit, true)}
-      />
-    ));
-  });
+      const hits = searchResults.hits;
+      return hits.map(hit => (
+        <ResultCard
+          key={hit.name + hit.version}
+          hit={hit}
+          onSelect={() => onSelect(hit, false)}
+          onSelectDev={() => onSelect(hit, true)}
+        />
+      ));
+    }
+  );
 
 const AlgoliaSearch = ({ query, onSelect }) => {
   const Content = ContentWithSelect(onSelect);
 
   return (
     <InstantSearch
-      searchState={{ query }}
       appId="OFCNCOG2CU"
       apiKey="6fe4476ee5a1832882e326b506d14126"
       indexName="npm-search"
+      searchState={{ query }}
     >
       <Configure hitsPerPage={5} />
       <VirtualSearchBox />
