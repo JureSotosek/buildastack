c8c94ded2f8f03ca510dc558bb9205d0edd94178
Jure Sotošek <jure.sotosek@gmail.com>
2019-10-18 17:14:07 +0100

    Revert "Update apolloClient.js"
    This reverts commit 82fe8a298a3fb174d96f9371a6e19c23a553c504.

--- src/lib/graphql/apolloClient.js
+++ src/lib/graphql/apolloClient.js
@@ -6,9 +6,7 @@ import Cookie from 'universal-cookie';
 
 const Cookies = new Cookie();
 
-const httpLink = new HttpLink({
-  uri: 'https://buildastack-5e801ac025.herokuapp.com'
-});
+const httpLink = new HttpLink({ uri: 'https://buildastack-api.herokuapp.com' });
 
 const authMiddleware = new ApolloLink((operation, forward) => {
   const token = Cookies.get('token');
